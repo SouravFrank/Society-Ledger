d4a1dc729929f9fb4131e38111438d8ded21c08d
Sourav Sadhukhan <ssadhukhan990@gmail.com>
2025-12-07 22:09:56 +0530

    Financal statement of Nov attached

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,59 +1,63 @@
-import type { FinancialStatement, MeetingMinute } from '@/lib/types';
+import type { FinancialStatement, MeetingMinute } from "@/lib/types";
 
 export const meetingMinutes: MeetingMinute[] = [
   {
-    date: '2025-09-07',
-    url: 'https://drive.google.com/file/d/1AFp_QCIU-Gx5sF2UfjVwDJiL0405Rax_/preview',
+    date: "2025-09-07",
+    url: "https://drive.google.com/file/d/1AFp_QCIU-Gx5sF2UfjVwDJiL0405Rax_/preview",
   },
   {
-    date: '2025-08-21',
-    url: 'https://drive.google.com/file/d/1MpzE9835NrbJG7I7wToOvPrV_lRYHIp1/preview',
+    date: "2025-08-21",
+    url: "https://drive.google.com/file/d/1MpzE9835NrbJG7I7wToOvPrV_lRYHIp1/preview",
   },
   {
-    date: '2025-08-28',
-    url: 'https://drive.google.com/file/d/1bu6EA6YUYCU_gVeI6q-_zvzXTmn1la0K/preview',
+    date: "2025-08-28",
+    url: "https://drive.google.com/file/d/1bu6EA6YUYCU_gVeI6q-_zvzXTmn1la0K/preview",
   },
   {
-    date: '2024-11-24',
-    url: 'https://drive.google.com/file/d/1GNQWlUXBWvbOZJ5QppNZd2SVqMcWcoty/preview',
+    date: "2024-11-24",
+    url: "https://drive.google.com/file/d/1GNQWlUXBWvbOZJ5QppNZd2SVqMcWcoty/preview",
   },
 ];
 
 export const financialStatements: FinancialStatement[] = [
- {
-    period: '2025-10',
-    url: 'https://drive.google.com/file/d/15yZqBNj_nKlbmRoNmbTZ1StZUlttU5HI/preview',
+  {
+    period: "2025-11",
+    url: "https://drive.google.com/file/d/1pKT9iLe4_q2YOcSJzTQVmW6kx8vq9NkZ/preview",
   },
   {
-    period: '2025-09',
-    url: 'https://drive.google.com/file/d/1YMy7uM8wfndFwjOYTIP7NsDRNyRlwXxG/preview',
+    period: "2025-10",
+    url: "https://drive.google.com/file/d/1_zN9L8IiRaqZwnqJoiv0lCXTAHFCPcRQ/preview",
   },
   {
-    period: '2025-08',
-    url: 'https://drive.google.com/file/d/1vh112uTHio9X7CzHmjyhEFXNBZluolrT/preview',
+    period: "2025-09",
+    url: "https://drive.google.com/file/d/1YMy7uM8wfndFwjOYTIP7NsDRNyRlwXxG/preview",
   },
   {
-    period: '2025-07',
-    url: 'https://drive.google.com/file/d/154F2OeVvB1EEPU-pEtdDV1hEgS4_BVHu/preview',
+    period: "2025-08",
+    url: "https://drive.google.com/file/d/1vh112uTHio9X7CzHmjyhEFXNBZluolrT/preview",
   },
   {
-    period: '2025-06',
-    url: 'https://drive.google.com/file/d/1zIkNm5b4RSSkhm7WyiCoJGPeGnYmQv2w/preview',
+    period: "2025-07",
+    url: "https://drive.google.com/file/d/154F2OeVvB1EEPU-pEtdDV1hEgS4_BVHu/preview",
   },
   {
-    period: '2025-05',
-    url: 'https://drive.google.com/file/d/11AkU18cXxPz9otmBDDHts2ADOeTJKP3C/preview',
+    period: "2025-06",
+    url: "https://drive.google.com/file/d/1zIkNm5b4RSSkhm7WyiCoJGPeGnYmQv2w/preview",
   },
   {
-    period: '2025-04',
-    url: 'https://drive.google.com/file/d/1W_Pj99czeXtjRzn7gnJmo4SbB6QE0dU0/preview',
+    period: "2025-05",
+    url: "https://drive.google.com/file/d/11AkU18cXxPz9otmBDDHts2ADOeTJKP3C/preview",
   },
   {
-    period: '2025-03',
-    url: 'https://drive.google.com/file/d/1nyEVre8OlRwhiLljCFTppg9bRS2kj55M/preview',
+    period: "2025-04",
+    url: "https://drive.google.com/file/d/1W_Pj99czeXtjRzn7gnJmo4SbB6QE0dU0/preview",
+  },
+  {
+    period: "2025-03",
+    url: "https://drive.google.com/file/d/1nyEVre8OlRwhiLljCFTppg9bRS2kj55M/preview",
   },
 ];
 
 export const ironGuardStatus: { url: string } = {
-  url: 'https://drive.google.com/file/d/1ThO1KcD05xYhpbrV141ErEqzQqRCYCor/preview',
+  url: "https://drive.google.com/file/d/1ThO1KcD05xYhpbrV141ErEqzQqRCYCor/preview",
 };
